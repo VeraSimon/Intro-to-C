29b06838398bf1d9add2d119e144d8572f17a3d0
Vera Simon <vera.simon01@gmail.com>
2019-03-25 21:09:10 -0700

    Strings string_length is working as expected, but having some issues returning the correct value for reverse_string. Will come back to it tomorrow.

--- strings/strings.c
+++ strings/strings.c
@@ -2,25 +2,48 @@
 #include <stdlib.h>
 
 /*
-    Given a character array s (as a pointer), return the number of 
+    Given a character array s (as a pointer), return the number of
     characters in the string.
-    
+
     Do not just use the `strlen` function from the standard libary.
 */
 int string_length(char *s)
 {
+    int counter = 0;
 
+    while (s[counter] != 0)
+    {
+        counter++;
+    }
+
+    return counter;
 }
 
 /*
-    Write a function that reverses the order of string s and outputs 
-    the reversed string to the input array rv. The rv array will have 
-    enough space for the reversed string. Don't forget to terminate 
+    Write a function that reverses the order of string s and outputs
+    the reversed string to the input array rv. The rv array will have
+    enough space for the reversed string. Don't forget to terminate
     the reversed string with a null character. Return the rv array.
 */
 char *reverse_string(char *rv, char *s)
 {
+    int sLen = string_length(s);
 
+    printf("rv start address: %c\n", &rv);
+
+    for (int i = sLen; i >= 0; i--)
+    {
+        printf("*s before: %c\n", *s);
+        rv[i] = *s;
+        printf("rv[i]: %c\n", rv[i]);
+        *s++;
+        printf("*s after: %c\n", *s);
+    }
+    rv[sLen + 1] = 0;
+
+    printf("rv end address: %i\n", rv);
+
+    return rv;
 }
 
 #ifndef TESTING
@@ -37,4 +60,3 @@ int main(void)
     return 0;
 }
 #endif
-    
