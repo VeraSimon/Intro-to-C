9904599aa1bc462746df58b3ab3a6b9ce3833c75
Vera Simon <vera.simon01@gmail.com>
2019-03-27 00:19:48 -0700

    FizzBuzz Cleaned the fizzbuzz function up a hair by recommendation of Luisan

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -18,15 +18,16 @@ int fizzbuzz(int n)
         {
             printf("Fizz\n");
         }
-        if (i % 3 != 0 && i % 5 == 0)
+        else if (i % 3 != 0 && i % 5 == 0)
         {
             printf("Buzz\n");
         }
-        if (i % 3 == 0 && i % 5 == 0)
+        else if (i % 3 == 0 && i % 5 == 0)
         {
             printf("FizzBuzz\n");
         }
-        if (i % 3 != 0 && i % 5 != 0)
+        // if (i % 3 != 0 && i % 5 != 0)
+        else
         {
             counter++;
         }
